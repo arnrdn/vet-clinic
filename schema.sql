9e521f27d7ed774aea0a751ef86f926253b686ec
rdnrn <rodina.arina00@gmail.com>
2022-02-28 22:43:52 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,10 +2,5 @@
 
 CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals (
-    id    INT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(250),
-    date_of_birth DATE,
-    escape_attempts
-);
+CREATE TABLE animals(id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(250), date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL, PRIMARY KEY(id));
 
