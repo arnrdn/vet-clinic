975dcb1acbc735452e597519b1cba9851ae8fa2b
rdnrn <rodina.arina00@gmail.com>
2022-03-02 21:38:22 +0300

    Add more data to the tables

--- data.sql
+++ data.sql
@@ -11,3 +11,37 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Angemon',TO_DATE('12/06/2005', 'DD/MM/YYYY'), 1, true, -45);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon',TO_DATE('07/06/2005', 'DD/MM/YYYY'), 7, true, 20.4);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom',TO_DATE('13/10/1998', 'DD/MM/YYYY'), 3, true, 17);
+
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
+
+/* Set species_id column */
+BEGIN;
+UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Digimon') WHERE name LIKE '%mon';
+SELECT * FROM animals; /* to check if changes has applied */
+UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Pokemon') WHERE species_id IS NULL;
+SELECT * FROM animals; /* to check if changes has applied */
+COMMIT;
+
+/* Set owner_id column */
+BEGIN;
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith') WHERE name = 'Agumon';
+SELECT * FROM animals; /* to check if changes has applied */
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell') WHERE name IN ('Gabumon', 'Pikachu');
+SELECT * FROM animals; /* to check if changes has applied */
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob') WHERE name IN ('Devimon', 'Plantmon');
+SELECT * FROM animals; /* to check if changes has applied */
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name IN ('Charmander', 'Plantmon', 'Blossom');
+SELECT * FROM animals; /* to check if changes has applied */
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name IN ('Angemon', 'Boarmon');
+SELECT * FROM animals; /* to check if changes has applied */
+COMMIT;
